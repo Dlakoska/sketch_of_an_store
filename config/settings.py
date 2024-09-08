22d259596e2ccdc42ae3da6652f0ab9852c8762f
Titov Evgenii <dlakoska@mail.ru>
2024-09-08 09:28:11 +0300

    создал базу данных и внес изменения в настройки для подключения

--- config/settings.py
+++ config/settings.py
@@ -52,8 +52,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'for_lesson',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432'
+
     }
 }
 
@@ -73,7 +78,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
